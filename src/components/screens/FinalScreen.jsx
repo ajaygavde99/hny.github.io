48c23fb35b7b16521c4b98b3116d26cc3e20ad0d
ajaygavde99 <ajaygavde99@gmail.com>
2025-12-31 18:34:36 +0530

    fireworks-js new3

--- src/components/screens/FinalScreen.jsx
+++ src/components/screens/FinalScreen.jsx
@@ -8,7 +8,55 @@ export default function FinalScreen() {
 
     useEffect(() => {
         if (fireworksRef.current) {
-            const fireworks = new Fireworks(fireworksRef.current, { /* options */ })
+            const fireworks = new Fireworks(fireworksRef.current, { /* options */ 
+                autoresize: true,
+                opacity: 0.5,
+                acceleration: 0.6,
+                friction: 0.97,
+                gravity: 1.5,
+                particles: 50,
+                traceLength: 3,
+                traceSpeed: 10,
+                explosion: 5,
+                intensity: 30,
+                flickering: 50,
+                lineStyle: 'round',
+                hue: {
+                    min: 0,
+                    max: 360
+                },
+                delay: {
+                    min: 30,
+                    max: 60
+                },
+                rocketsPoint: {
+                    min: 50,
+                    max: 50
+                },
+                lineWidth: {
+                    explosion: {
+                    min: 1,
+                    max: 3
+                    },
+                    trace: {
+                    min: 1,
+                    max: 2
+                    }
+                },
+                brightness: {
+                    min: 50,
+                    max: 80
+                },
+                decay: {
+                    min: 0.015,
+                    max: 0.03
+                },
+                mouse: {
+                    click: false,
+                    move: false,
+                    max: 1
+                }
+            })
             fireworks.start()
             return () => fireworks.stop()
         }
