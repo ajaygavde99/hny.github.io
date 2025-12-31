7f0ed5fc7340f7b5151f6697abeedec559a9ba18
ajaygavde99 <ajaygavde99@gmail.com>
2025-12-31 17:46:00 +0530

    fireworks 3

--- fireworks.js
+++ fireworks.js
@@ -1,171 +1,361 @@
 // Fireworks animation script
-window.triggerFireworks = function triggerFireworks() {
-    const canvas = document.getElementById('fireworks');
-    if (!canvas) return;
-    const gl = canvas.getContext('webgl2');
-    if (!gl) { alert('WebGL 2 required'); return; }
+const canvas = document.getElementById('fireworks');
+const ctx = canvas.getContext('2d');
 
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-    const vs = `#version 300 es
-    in vec2 a;
-    void main(){ gl_Position=vec4(a,0,1); }`;
+function createFirework() {
+			window.oncontextmenu = function () { return false; } // Block menu
 
-    const fs = `#version 300 es
-    precision highp float;
-    uniform vec3  iResolution;
-    uniform float iTime;
-    uniform vec4  iMouse;
-out vec4 fragColor;
+			var canvas = document.getElementById('canvas'); // Get element by ID canvas
+			var ctx = canvas.getContext('2d'); 				// Get concent of canvas 2d graphics
+			var frameRate = 60.0;							// Frame rate per second use in loop
+			var frameDelay = 1000.0/frameRate;				// Frame Delay per second like latency
 
-/* -----------  YOUR ORIGINAL SHADER  ------------ */
-#define rad(x) radians(x)
-#define np 50.
-#define snp 40.
-#define spawn 1
-#define trail 1
+			var clientWidth = innerWidth;					// Clients Width of web screen
+			var clientHeight = innerHeight;					// Clients height of web screen
+			var timer = 0;									// Timer is ticker, how many ticks per round
+			var x = 0;										// Mouse x coordinates
+			var y = 0;										// Mouse y coordinates
 
-vec2 N22(vec2 p){
-    vec3 a = fract(p.xyx*vec3(123.34, 234.34, 345.65));
-    a += dot(a, a+34.45);
-    return fract(vec2(a.x*a.y, a.y*a.z));
+			canvas.width = clientWidth;						// Set canvas width to user width
+			canvas.height = clientHeight;					// Set canvas height to user height
+
+			var TimedFirework = 1000;						// Repeat Firework every x MS
+			var LimiterTicker = 0;							// 
+			var fireworks = [];								// Array with starting fireworks
+			var particles = [];								// Array with particles
+			var typecount = 1;								// Variable to change firework type
+			var sparks = [];								// Array for sparkles drops
+			var num = 1;									// number of color
+			var colorchanger = 0;							// colorchange timer
+
+			// Function to calculate distance = Simple Pythagorean theorem
+			function distance(px1, py1, px2, py2) {
+				xdis = px1 - px2;
+				ydis = py1 - py2;
+				return Math.sqrt((xdis*xdis) + (ydis*ydis));
+			}
+
+			// My own created function to get angle from point to point
+			function getAngle(posx1, posy1, posx2, posy2) {
+				if (posx1 == posx2) { if (posy1 > posy2) { return 90; } else { return 270; } }
+				if (posy1 == posy2) { if (posy1 > posy2) { return 0; } else { return 180; } }
+
+				var xDist = posx1 - posx2;
+				var yDist = posy1 - posy2;
+
+				if (xDist == yDist) { if (posx1 < posx2) { return 225; } else { return 45; } }
+				if (-xDist == yDist) { if (posx1 < posx2) { return 135; } else { return 315; } }
+
+				if (posx1 < posx2) {
+					return Math.atan2(posy2-posy1, posx2-posx1)*(180/Math.PI) + 180;
+				} else {
+					return Math.atan2(posy2-posy1, posx2-posx1)*(180/Math.PI) + 180;
+				}
+			}
+
+			// My function to create random number
+			function random(min, max, round) {
+				if (round == 'round') {
+					return Math.round(Math.random() * (max - min) + min);
+				} else {
+					return Math.random() * (max - min) + min;
+				}
+			}
+
+			// Function to choose one of these best colors
+			function colors() {
+				if (timer > colorchanger) { num = random(0, 7, 'round'); colorchanger = timer + (500); }
+				switch(num) {
+					case 1: return '#ff0000'; break;
+					case 2: return '#ffff00'; break;
+					case 3: return '#00ff00'; break;
+					case 4: return '#00ffff'; break;
+					case 5: return '#0000ff'; break;
+					case 6: return '#ff00ff'; break;
+					case 7: return '#ffac00'; break;
+				}
+			}
+
+			// Function to make firework
+			createFirework = function() {
+				firework = new Firework();
+
+				firework.x = firework.sx = clientWidth/2;
+				firework.y = firework.sy = clientHeight;
+
+				firework.color = colors();
+
+				if (x != 0 && y != 0) {
+					firework.tx = x;
+					firework.ty = y;
+					x = y = 0;
+				} else {
+					firework.tx = random(400, clientWidth-400);
+					firework.ty = random(0, clientHeight / 2);
+				}
+
+				var angle = getAngle(firework.sx, firework.sy, firework.tx, firework.ty);
+
+				firework.vx = Math.cos(angle * Math.PI/180.0);
+				firework.vy = Math.sin(angle * Math.PI/180.0);
+
+				fireworks.push(firework);
+			}
+
+			// Function to start Firework
+			Firework = function() {
+
+				this.x = 0;
+				this.y = 0;
+				this.sx = 0;
+				this.sy = 0;
+				this.tx = 0;
+				this.ty = 0;
+				this.vx = 0;
+				this.vy = 0;
+				this.color = 'rgb(255,255,255)';
+				this.dis = distance(this.sx, this.sy, this.tx, this.ty);
+				this.speed = random(700, 1100);
+				this.gravity = 1.5;
+				this.ms = 0;
+				this.s = 0;
+				this.del = false;
+
+				this.update = function(ms) {
+					this.ms = ms / 1000;
+
+					if (this.s > 2000/ms) {
+						createParticles(typecount, 30, this.x, this.y, this.color);
+						this.del = true;
+					} else {
+						this.speed *= 0.98;
+						this.x -= this.vx * this.speed * this.ms;
+						this.y -= this.vy * this.speed * this.ms - this.gravity;
+					}
+
+					this.s++;
+				}
+
+				this.draw = function() {
+					ctx.beginPath();
+					ctx.fillStyle = this.color;
+					ctx.arc(this.x, this.y, 1, 0, 2*Math.PI);
+					ctx.fill();
+				}
+			}
+
+			// Function to create array particles
+			createParticles = function(type, count, pox, poy, color) {
+				for (var i = 0; i < count; i++) {
+					par = new Particles();
+					par.type = type;
+
+					par.color = color;
+					par.x = pox;
+					par.y = poy;
+
+					var angle = random(0, 360);
+					par.vx = Math.cos(angle * Math.PI/180.0);
+					par.vy = Math.sin(angle * Math.PI/180.0);
+
+					particles.push(par);
+				};
+			}
+
+			// Function to make particles
+			Particles = function() {
+
+				this.x = 0;
+				this.y = 0;
+				this.vx = 0;
+				this.vy = 0;
+				this.speed = random(200, 500);
+				this.gravity = 1;
+				this.wind = 0;
+				this.type = 1;
+				this.opacity = 1;
+				this.s = 0;
+				this.scale = 1;
+				this.color = '#FFF';
+				this.del = false;
+
+				this.update = function(ms) {
+					this.ms = ms / 1000;
+
+					if (this.s > 900/ms) { if (this.opacity - 0.05 < 0) { this.opacity = 0; } else { this.opacity -= 0.05; } }
+
+					if (this.type == 1) {
+						this.speed *= 0.96;
+						this.x -= this.vx * this.speed * this.ms + this.wind;
+						this.y -= this.vy * this.speed * this.ms - this.gravity;
+					} else if (this.type == 2) {
+						if (this.s < 800/ms) { this.scale += 0.1; } else { this.scale -= 0.2; }
+						this.speed *= 0.96;
+						this.x -= this.vx * this.speed * this.ms + this.wind;
+						this.y -= this.vy * this.speed * this.ms - this.gravity;
+					} else if (this.type == 3) {
+						this.speed *= 0.95;
+						this.x -= this.vx * this.speed * this.ms + this.wind;
+						this.y -= this.vy * this.speed * this.ms;
+					} else if (this.type == 4) {
+						this.speed *= 0.96;
+						this.x -= this.vx * this.speed * this.ms + this.wind;
+						this.y -= this.vy * this.speed * this.ms - this.gravity;
+
+						spark = new Sparkler();
+						spark.x = this.x;
+						spark.y = this.y;
+						spark.vx = Math.cos(random(0, 360, 'round') * (Math.PI/180))*1.05;
+						spark.vy = Math.sin(random(0, 360, 'round') * (Math.PI/180))*1.05;
+						spark.tx = this.x;
+						spark.ty = this.y;
+						spark.color = this.color;
+						spark.limit = random(4, 10, 'round');
+						sparks.push(spark);
+					} else {
+
+					}
+
+					this.s++;
+				}
+
+				this.draw = function() {
+					ctx.save();
+					ctx.globalAlpha = this.opacity;
+					ctx.fillStyle = this.color;
+					ctx.strokeStyle = this.color;
+
+					if (this.type == 1) {
+						ctx.beginPath();
+						ctx.arc(this.x, this.y, 1.5, 0, 2*Math.PI);
+						ctx.fill();
+					} else if (this.type == 2) {
+						ctx.translate(this.x, this.y);
+						ctx.scale(this.scale, this.scale);
+						ctx.beginPath();
+						ctx.fillRect(0, 0, 1, 1);
+					} else if (this.type == 3) {
+						ctx.beginPath();
+						ctx.moveTo(this.x, this.y);
+						ctx.lineTo(this.x - this.vx * 10, this.y - this.vy * 10);
+						ctx.stroke();
+					} else if (this.type == 4) {
+						ctx.beginPath();
+						ctx.arc(this.x, this.y, 1.5, 0, 2*Math.PI);
+						ctx.fill();
+					} else {
+						ctx.arc(this.x, this.y, 1, 0, 2*Math.PI);
+						ctx.fill();
+					}
+
+					ctx.closePath();
+					ctx.restore();
+				}
+			}
+
+			// Function for sparkler type of firework
+			Sparkler = function() {
+
+				this.x = 0;
+				this.y = 0;
+				this.tx = 0;
+				this.ty = 0;
+				this.limit = 0;
+				this.color = 'red';
+
+				this.update = function() {
+					this.tx += this.vx;
+					this.ty += this.vy;
+
+					this.limit--;
+				}
+
+				this.draw = function() {
+					ctx.beginPath();
+					ctx.moveTo(this.x, this.y);
+					ctx.lineTo(this.tx, this.ty);
+					ctx.lineWidth = 1;
+					ctx.strokeStyle = this.color;
+					ctx.stroke();
+					ctx.closePath();
+				}
+			}
+
+			text = function() {
+				ctx.beginPath();
+				ctx.fillStyle = 'white';
+				ctx.font = "14px arial";
+				ctx.fillText("You can change the Firework style with right click and fire with left click on mouse.", 2, clientHeight-2);
+			}
+
+			// Mouse coordinates to fire
+			canvas.addEventListener('mousedown', function(evt) {
+				evt = evt || window.event;
+				var button = evt.which || evt.button;
+			    if (button == 1) {
+			    	// If button is first (left) on mouse
+					x = evt.clientX; y = evt.clientY; createFirework();
+				} else { 
+					// If button is second (right) on mouse
+					if (typecount == 4) { typecount = 1; } else { typecount++; }
+				}
+			});
+
+			update = function(frame) {
+
+				// text to controll firework
+				text();
+
+				// Every tick clear screen with black rectangle with opacity 0.15
+				ctx.globalAlpha = 1;
+				ctx.fillStyle = 'rgba(0, 0, 0, 0.15)';
+				ctx.fillRect(0, 0, clientWidth, clientHeight);
+
+
+				if (timer > LimiterTicker) {
+					// Creating array with starting Firework
+					createFirework();
+
+					LimiterTicker = timer + (TimedFirework / frame);
+				}
+
+				var i = fireworks.length;
+				while(i--) {
+					// Progress starting Fireworks
+					if (fireworks[i].del == true) { fireworks.splice(i, 1); } else {
+						fireworks[i].update(frame);
+						fireworks[i].draw();
+					}
+				}
+
+				var i = particles.length;
+				while(i--) {
+					// Progress particles
+					if (particles[i].opacity == 0) { particles.splice(i, 1); } else {
+						particles[i].update(frame);
+						particles[i].draw();
+					}
+				}
+
+				var i = sparks.length;
+				while(i--) {
+					// Progress sparks
+					if (sparks[i].limit < 0) { sparks.splice(i, 1); } else {
+						sparks[i].update(frame);
+						sparks[i].draw();
+					}
+				}
+
+				timer++;
+			}
+
+			var main = setInterval(function() { update(frameDelay); }, frameDelay);
+
 }
-float hash(vec2 uv){
-    return fract(sin(dot(uv,vec2(154.45,64.548))) * 124.54); 
-}
 
-vec3 particle(vec2 st, vec2 p, float r, vec3 col){
-    float d = length(st-p);
-    d = smoothstep(r, r-2.0/iResolution.y, d);
-    return d*col;
-}
-vec3 burst(vec2 st, vec2 pos, float r, vec3 col, int heart) {
-    st -= pos;
-    if (heart==1) st.y -= sqrt(abs(st.x))*0.1;
-    r *=0.6*r;
-    return (r/dot(st, st))*col*0.6;
-}
-
-vec2 get_pos(vec2 u, vec2 a, vec2 p0, float t, float ang){
-    ang = rad(ang);
-    vec2 d = p0 + vec2(u.x*cos(ang), u.y*sin(ang))*t + 0.5*a*t*t;
-    return d;
-}
-vec2 get_velocity(vec2 u, vec2 a, float t, float ang){
-    ang = rad(ang);
-    return vec2(u.x*cos(ang), u.y*sin(ang)) + a*t;
-}
-
-void mainImage( out vec4 fragColor, in vec2 fragCoord ){
-    vec2 uv = (2.*fragCoord-iResolution.xy)/iResolution.y;
-    vec3 col = vec3(0.0);
-    float t = mod(iTime, 10.);
-    
-    float r = 0.04;
-    vec2 u = vec2(5.);
-    vec2 a = vec2(0.0, -9.8);
-    float ang = 75.0;
-
-    vec3 p1 = vec3(0.0);
-    
-    for (float i=0.; i<np; i++){
-        vec2 rand = N22(vec2(i));
-        vec2 ip = vec2(sin(15.*rand.x), -1.+r);
-        u = vec2(sin(5.*rand.x), 5.+sin(4.*rand.y));
-        float t1 = t-i/5.;
-        vec2 s = get_pos(u, a, ip, t1, ang);
-        vec2 v = get_velocity(u, a, t1, ang);
-        float Tf = 2.0*u.y*sin(rad(ang))/abs(a.y);
-        vec2 H = get_pos(u, a, ip, Tf/2.0, ang);
-        vec3 pcol = vec3(sin(22.*rand.x), sin(5.*rand.y), sin(1.*rand.x));
-
-        if (v.y<-0.5){ r=0.0; }
-        p1 += burst(uv, s, r, pcol, 0);
-
-        if (trail==1){
-            for (float k=4.0; k>0.0; k--){
-                vec2 strail = get_pos(u, a, ip, t1-(k*0.02), ang);
-                p1 += burst(uv, strail, v.y<-0.5?0.0:r-(k*0.006), pcol, 0);
-            }
-        }
-        
-        if (v.y<=0.0 && t1>=Tf/2.0 && spawn==1){
-            for (float j=0.0; j<snp; j++){
-                vec2 rand2 = N22(vec2(j));
-                float ang2 = (j*(360./snp));
-                r = 0.035;
-                r -= (t1-Tf*0.5)*0.04;
-                float x = cos(rad(ang2));
-                float y = sin(rad(ang2));
-                y = y + abs(x) * sqrt( (8.- abs(x))/50.0 );
-                vec2 heart = vec2(x*x + y*y)*(0.4/(t1*sqrt(t1)));
-                vec2 S = get_pos(heart, a*0.03, H, t1-(Tf/2.), ang2);
-                pcol = vec3(sin(8.*rand2.x), sin(6.*rand2.y), sin(2.*rand2.x));
-                p1 += burst(uv, S, max(0.0,r), pcol, 0);
-            }
-        } 
-    }
-    
-    float stars = pow(hash(uv),200.) * 0.5;
-    col = p1;
-    vec3 night = vec3(0.06, 0.02, 0.18)*vec3(uv.y*0.5+0.5)+vec3(stars)*vec3(uv.y*0.5+0.5);
-    col += night*(1.0-p1);
-    fragColor = vec4(col,1.0);
-}
-/* ----------------------------------------------- */
-
-void main(){ mainImage(fragColor, gl_FragCoord.xy); }
-`;
-
-function compile(type, src) {
-  const s = gl.createShader(type);
-  gl.shaderSource(s, src);
-  gl.compileShader(s);
-  if (!gl.getShaderParameter(s, gl.COMPILE_STATUS))
-    console.error(gl.getShaderInfoLog(s));
-  return s;
-}
-const prog = gl.createProgram();
-gl.attachShader(prog, compile(gl.VERTEX_SHADER, vs));
-gl.attachShader(prog, compile(gl.FRAGMENT_SHADER, fs));
-gl.linkProgram(prog);
-gl.useProgram(prog);
-
-const buf = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, buf);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-1,-1, 1,-1, -1,1, 1,1]), gl.STATIC_DRAW);
-const loc = gl.getAttribLocation(prog, 'a');
-gl.enableVertexAttribArray(loc);
-gl.vertexAttribPointer(loc, 2, gl.FLOAT, 0, 0, 0);
-
-const uni = {
-  res : gl.getUniformLocation(prog, 'iResolution'),
-  time: gl.getUniformLocation(prog, 'iTime'),
-  mouse: gl.getUniformLocation(prog, 'iMouse')
-};
-
-function resize() {
-  canvas.width  = window.innerWidth;
-  canvas.height = window.innerHeight;
-  gl.viewport(0, 0, canvas.width, canvas.height);
-}
-window.addEventListener('resize', resize);
-resize();
-
-let mx = 0, my = 0, mz = 0;
-canvas.addEventListener('mousemove', e => { mx = e.clientX; my = canvas.height - e.clientY; });
-canvas.addEventListener('mousedown', () => mz = 1);
-canvas.addEventListener('mouseup',   () => mz = 0);
-
-function frame(t) {
-  gl.uniform3f(uni.res, canvas.width, canvas.height, 1.0);
-  gl.uniform1f(uni.time, t * 0.001);
-  gl.uniform4f(uni.mouse, mx, my, mz, 0);
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-  requestAnimationFrame(frame);
-}
+function animate() {
 // The WebGL shader-based heart fireworks are initialized and animated automatically above.
-
-    requestAnimationFrame(frame);
 }
